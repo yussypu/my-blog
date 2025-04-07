3c82403e0ee6f22b04a5a7707a234842bc667866
Muhammad Yahya Qureshi <yahya@Muhammads-MacBook-Air.local>
2025-04-08 01:40:35 +0200

    metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Yahya Ehsan',
+  author: 'Yahya Ehsan',
+  headerTitle: 'Blog',
+  description: '2nd Year Computer Science Student exploring tech',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
